e0c27e40eb3cf30775e19ca8121671104a65b25b
dbszl3 <yoonji0811@naver.com>
2026-08-05 21:58:37 +0900

    🔧 Settings: 프로젝트 공통 레이아웃 및 폰트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import RootLayout from "./layout/RootLayout";
-import Main from "./pages/Main/Main";
+import Home from "./pages/Home/Home";
+import Mission from "./pages/Mission/Mission";
+import Product from "./pages/Product/Product";
+import Mypage from "./pages/Mypage/Mypage";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<RootLayout />}>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
