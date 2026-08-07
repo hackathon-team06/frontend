c62de3713004a476492d08a19ff0dd809a2eda2e
dbszl3 <yoonji0811@naver.com>
2026-08-07 13:31:42 +0900

    ♻️ Refactor: 일지 모달 날짜 표시

--- src/components/common/RecordModal/RecordModal.jsx
+++ src/components/common/RecordModal/RecordModal.jsx
@@ -8,6 +8,12 @@ export default function RecordModal({ onClose }) {
 
   const record = records[page];
 
+  const recordDate = new Date(); // 오늘 날짜
+  recordDate.setDate(recordDate.getDate() - 2);
+
+  {/* 오늘이 4일차이기 때문에 4일차 기준 이틀 전으로 날짜 표시 */}
+  const showingDate = `${recordDate.getMonth() + 1}/${recordDate.getDate()}`;
+
   return (
     <div
       className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-[1.5px]"
@@ -20,17 +26,22 @@ export default function RecordModal({ onClose }) {
         {/* 이전 버튼 */}
         {page === 1 && (
           <img
-            src={prevButton} 
+            src={prevButton}
             className="cursor-pointer absolute top-1/2 -translate-y-1/2 -left-9"
-            onClick={() => setPage(0)} />
+            onClick={() => setPage(0)}
+          />
         )}
 
         {/* 모달 */}
         <div className="w-[306px] h-[370px] rounded-2xl bg-white outline-[3px] outline-[#78BAA9] outline-offset-[-1.5px]">
           <main className="flex flex-col">
-            <p className="mt-[30px] ml-[14px] text-base font-semibold text-[#78BAA9]">
-              {record.title}
-            </p>
+            <header className="flex justify-between">
+              <p className="mt-[30px] ml-[14px] text-base font-semibold text-[#78BAA9]">
+                {record.title}
+              </p>
+              <p className="logo-font mt-[30px] mr-[21px]">{showingDate}</p>
+            </header>
+
             <p className="mt-9 ml-[14px] mr-[14px] whitespace-pre-line text-base font-semibold leading-5 text-stone-950">
               {record.description}
             </p>
@@ -49,7 +60,8 @@ export default function RecordModal({ onClose }) {
           <img
             src={nextButton}
             className="cursor-pointer absolute top-1/2 -translate-y-1/2 -right-9"
-            onClick={() => setPage(1)} />
+            onClick={() => setPage(1)}
+          />
         )}
       </div>
     </div>
