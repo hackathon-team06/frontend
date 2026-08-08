e43f5ca5523a3fc18e9b3bbea816a6f5c9a68a0a
dbszl3 <yoonji0811@naver.com>
2026-08-08 19:33:15 +0900

    🎨 Design: 마이페이지 및 스탬프 화면 UI 수정

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -1,6 +1,6 @@
-import checkIcon from "../../assets/icons/check_icon.svg";
+import profile from "../../assets/images/profile.svg";
 
-import Stamp from "../../components/common/StampCard/StampCard";
+import StampCard from "../../components/common/StampCard/StampCard";
 
 import { useNavigate } from "react-router-dom";
 
@@ -9,88 +9,64 @@ const testUser = [
     name: "수분남",
     age: "21~25세",
     skinType: "지성피부",
-    purpose: "수분 챙기기",
+    purpose: "촉촉한 피부",
   },
 ];
 
-const week = [
-  { day: "월", completed: true },
-  { day: "화", completed: true },
-  { day: "수", completed: true },
-  { day: "목", completed: false },
-  { day: "금", completed: false },
-  { day: "토", completed: false },
-  { day: "일", completed: false },
-];
-
-export default function MyPage() {
-  const today = new Date();
-
-  const dayList = ["일", "월", "화", "수", "목", "금", "토"];
-  const day = dayList[today.getDay()];
+export default function Mypage() {
 
   const navigate = useNavigate();
 
   return (
-    <div>
-      <p className="text-black text-xl font-medium ml-[149px] mt-[53px]">
-        마이페이지
-      </p>
-      <div className="w-20 h-20 rounded-full bg-[#D9D9D9] mt-[39px] ml-[153px]" />
-      {testUser.map((user) => (
-        <div
-          key={user.name}
-          className="flex flex-col items-center gap-4 mt-[22px]"
-        >
-          <p className="text-stone-950 text-base font-medium">{user.name}</p>
-          <div className="flex gap-[6px] items-center">
-            <p className="text-stone-950 text-xs font-semibold">{user.age}</p>
-            <div className="w-[1px] h-3 bg-[#BABABA]" />
-            <p className="text-stone-950 text-xs font-semibold">
-              {user.skinType}
-            </p>
-            <div className="w-[1px] h-3 bg-[#BABABA]" />
-            <p className="text-stone-950 text-xs font-semibold">
-              {user.purpose}
-            </p>
+    <main className="bg-[#EFF7F7] h-[100vh]">
+      <div className="flex flex-col items-center">
+        <p className="text-black text-xl font-medium pt-[62px]">마이페이지</p>
+        {/* 사용자 정보 */}
+        <section className="mt-[65px] w-[352px] h-[134px] bg-white rounded-2xl shadow-[0px_-1px_3px_0px_rgba(0,0,0,0.05)]">
+          {testUser.map((user) => (
+            <div className="flex flex-col items-start">
+              {/* 프로필 + 이름 */}
+              <div className="flex gap-[15px] mt-[24px] ml-[19px]">
+                <img src={profile} />
+                <p className="text-stone-950 text-lg font-semibold mt-[3px]">
+                  {user.name}
+                </p>
+              </div>
+              <div className="w-80 h-[1px] bg-[#C3C3C3] ml-[16px] mt-[22px]" />
+              {/* 나이 + 피부 타입 + 목적 */}
+              <div className="flex ml-[19px] mt-[10px] items-center">
+                <p className="text-stone-950 text-xs font-semibold">
+                  {user.age}
+                </p>
+                <div className="w-[1px] h-3 bg-[#C3C3C3] ml-[57px]" />
+                <p className="text-stone-950 text-xs font-semibold ml-[12px]">
+                  {user.skinType}
+                </p>
+                <div className="w-[1px] h-3 bg-[#C3C3C3] ml-[61px]" />
+                <p className="text-stone-950 text-xs font-semibold ml-[12px]">
+                  {user.purpose}
+                </p>
+              </div>
+            </div>
+          ))}
+        </section>
+        {/* 포인트 */}
+        <section className="relative mt-[14px] w-[352px] h-[106px] bg-white rounded-2xl shadow-[0px_-1px_3px_0px_rgba(0,0,0,0.05)]">
+          <div className="flex flex-col gap-[15px]">
+            <p className="text-black text-base font-medium mt-4 ml-3">현재 포인트</p>
+            <p className="ml-3 text-[#65DBBE] text-xl font-medium">1163<span className="text-black text-xl font-medium">P</span></p>
+            <p className="absolute bottom-[7px] right-4 text-black text-xs font-normal">제품 보러가기</p>
           </div>
-        </div>
-      ))}
-      <p className="mt-[43px] ml-[19px] text-black text-sm font-semibold">
-        14일 유수분 밸런스 코스 (4일 차)
-      </p>
-      {week.map((item) => (
-        <span
-          key={item.day}
-          className={`inline-block text-sm font-semibold ml-[37px] mt-[23px]
-            ${item.day === day ? "bg-[#65DBBE] size-5 rounded-lg text-white inline-flex items-center justify-center leading-none" : "text-stone-950"}
-          `}
-        >
-          {item.day}
-        </span>
-      ))}
-      <div className="relative w-[340px] h-8 bg-[#C3C3C3] rounded-[20px] ml-[20px] mt-[14px]">
-        {/* 초록 진행바 */}
-        <div className="absolute left-0 top-0 w-[192px] h-full bg-[#65DBBE] rounded-[20px]">
-          <img src={checkIcon} className="absolute left-[10px]" />
-          <img src={checkIcon} className="absolute left-[57px]" />
-          <img src={checkIcon} className="absolute left-[104px]" />
+        </section>
+        <p className="text-black text-base font-semibold self-start mt-[22px] ml-[19px]">스탬프 총 4개 수집 완료</p>
+        <div className="flex flex-wrap gap-2 ml-[19px] mt-4">
+          <StampCard date="4/7" day="21DAY" />
+          <StampCard date="5/1" day="28DAY" onClick={() => navigate("/mypage/stamp")}/>
+          <StampCard date="6/3" day="7DAY" />
+          <StampCard date="6/12" day="28DAY" />
+          <StampCard date="7/26" day="진행중.." />
         </div>
       </div>
-      <p className="text-black text-lg font-semibold mt-13 ml-[19px]">
-        스탬프 총 4개 수집 완료
-      </p>
-      <div className="flex flex-wrap gap-[8px] ml-[19px] mt-[31px] mb-[10px]">
-        <Stamp date="4/7" day="21DAY" />
-        <Stamp
-          date="5/1"
-          day="28DAY"
-          onClick={() => navigate("/mypage/stamp")}
-        />
-        <Stamp date="6/3" day="7DAY" />
-        <Stamp date="6/12" day="28DAY" />
-        <Stamp date="7/26" day="진행중.." />
-      </div>
-    </div>
+    </main>
   );
 }
