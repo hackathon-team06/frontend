b64bdaf5d97a00fb7fa81d4b58fde5dddc4e5f4f
dbszl3 <yoonji0811@naver.com>
2026-08-13 16:36:14 +0900

    ✨ Feat: 온보딩 루틴 페이지 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "./layout/RootLayout";
 import Login from "./pages/Login/Login";
 import Onboarding from "./pages/Onboarding/Onboarding";
+import Result from "./pages/Onboarding/Result";
+import Complete from "./pages/Onboarding/Complete";
+import RoutineSetting from "./pages/Onboarding/RoutineSetting";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route element={<RootLayout />}>
           <Route path="/" element={<Login />} />
           <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/onboarding/:skinType" element={<Result />} />
+          <Route path="/onboarding/complete" element={<Complete />} />
+          <Route path="/onboarding/routine-setting" element={<RoutineSetting />} />
         </Route>
       </Routes>
     </BrowserRouter>
