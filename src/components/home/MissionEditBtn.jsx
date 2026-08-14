bf7797da7e152203bbddb99b217ab34259564474
dbszl3 <yoonji0811@naver.com>
2026-08-14 21:16:41 +0900

    ✨ feat: 미션 수정 및 제거 기능 구현

--- src/components/home/MissionEditBtn.jsx
+++ src/components/home/MissionEditBtn.jsx
@@ -1,6 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 export default function MissionEditBtn() {
+
+  const navigate = useNavigate();
+
   return (
-    <button className="w-20 h-7.5 bg-neutral-50 rounded-3xl outline-2 outline-emerald-300 text-sm text-emerald-300 font-bold cursor-pointer">
+    <button onClick={() => navigate("/edit")} className="w-20 h-7.5 bg-neutral-50 rounded-3xl outline-2 outline-emerald-300 text-sm text-emerald-300 font-bold cursor-pointer">
       미션 수정
     </button>
   );
