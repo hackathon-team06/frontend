5c9d81c2f6fa36175c5da21fb768ee74261e69ef
loo-08 <zxzxhr100@gmail.com>
2026-08-16 10:55:26 +0900

    🎨 Design: 홈 미션 캐릭터 이미지 및 문구 색상 변경

--- src/components/home/SetUpCharacterSection.jsx
+++ src/components/home/SetUpCharacterSection.jsx
@@ -1,10 +1,10 @@
-import character from "../../assets/images/missioncharacter.svg";
+import character from "../../assets/images/mission_character.png";
 
 function SetUpCharacterSection() {
   return (
-    <div className="flex flex-col gap-2.25 items-center justify-center pt-14 pb-18.5">
-      <img className="size-24" src={character} alt="캐릭터" />
-      <div className="text-lg font-semibold">
+    <div className="flex flex-col gap-3 items-center justify-center pt-14 pb-18.5">
+      <img className="w-[57px] h-[77px]" src={character} alt="캐릭터" />
+      <div className="text-lg font-semibold text-[#2E4972]">
         아직 미션이 정해지지 않았어요..
       </div>
     </div>
