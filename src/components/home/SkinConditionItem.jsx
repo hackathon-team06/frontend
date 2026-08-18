16c4f4e986f217438de7203168d2657c1a2f33d5
dbszl3 <yoonji0811@naver.com>
2026-08-18 10:49:39 +0900

    ✨ Feat: 저녁 미션 상태 옵션 조회 API 연동

--- src/components/home/SkinConditionItem.jsx
+++ src/components/home/SkinConditionItem.jsx
@@ -3,16 +3,24 @@ import { skinConditionIcons } from "../../constants/home/skinConditionIcons";
 function SkinConditionItem({ data, isSelected, onClick }) {
   return (
     <div
-      className={`rounded-2xl border-2 ${isSelected ? "border-[#65DBBE]" : "border-[#EBEBEB]"}`}
+      className={`rounded-2xl border-2 ${
+        isSelected ? "border-[#65DBBE]" : "border-[#EBEBEB]"
+      }`}
     >
       <button
         onClick={onClick}
-        className="flex items-center px-3 py-2 gap-1 cursor-pointer "
+        className="flex cursor-pointer items-center gap-1 px-3 py-2"
       >
         {data.icon && (
-          <img src={skinConditionIcons[data.icon]} className="w-4 h-4" />
+          <img
+            src={skinConditionIcons[data.icon]}
+            className="h-4 w-4"
+          />
         )}
-        <span className="text-base font-semibold">{data.label}</span>
+
+        <span className="text-base font-semibold">
+          {data.label}
+        </span>
       </button>
     </div>
   );
