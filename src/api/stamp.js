490fa3583f91f18c67e7c90c662b7e878e6c5f0c
dbszl3 <yoonji0811@naver.com>
2026-08-18 16:53:15 +0900

    ✨Feat: 스탬프북 카드 조회 API 추가

--- src/api/stamp.js
+++ src/api/stamp.js
@@ -11,3 +11,10 @@ export const getStampCalendar = async (year, month) => {
 
   return response.data;
 };
+
+// 마이페이지 스탬프북 카드 조회
+export const getStampBooks = async () => {
+  const response = await api.get("/api/stamps/books");
+
+  return response.data;
+};
