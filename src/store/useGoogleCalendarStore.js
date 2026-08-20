1242f852fba64ecbe57a9138a28e3c33b8775fbc
loo-08 <zxzxhr100@gmail.com>
2026-08-20 15:27:08 +0900

    :bug:Fix: 구글 캘린더 연동 상태를 서버 값으로 판단하도록 변경

--- src/store/useGoogleCalendarStore.js
+++ src/store/useGoogleCalendarStore.js
@@ -1,31 +1,10 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
-import useAuthStore from "./useAuthStore";
+const useGoogleCalendarStore = create((set) => ({
+  justConnected: false,
 
-const useGoogleCalendarStore = create(
-  persist(
-    (set) => ({
-      // 데이터
-      isConnected: false,
-      justConnected: false,
-
-      // 함수
-      connect: () => set({ isConnected: true, justConnected: true }),
-      clearJustConnected: () => set({ justConnected: false }),
-      clear: () => set({ isConnected: false, justConnected: false }),
-    }),
-    {
-      name: "google-calendar-storage",
-      partialize: (state) => ({ isConnected: state.isConnected }),
-    },
-  ),
-);
-
-useAuthStore.subscribe((state, prevState) => {
-  if (prevState.accessToken && !state.accessToken) {
-    useGoogleCalendarStore.getState().clear();
-  }
-});
+  connect: () => set({ justConnected: true }),
+  clearJustConnected: () => set({ justConnected: false }),
+}));
 
 export default useGoogleCalendarStore;
