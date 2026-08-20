d6ecca0f4bb734b36e8ac4726c5aeeb008179139
dbszl3 <yoonji0811@naver.com>
2026-08-20 14:12:38 +0900

    ✨ Feat: 온보딩 건너뛰기 기능 제거

--- src/components/common/OnboardingStatus/OnboardingStatus.jsx
+++ src/components/common/OnboardingStatus/OnboardingStatus.jsx
@@ -4,8 +4,6 @@ import backButton from "../../../assets/images/back_button.svg";
 import union from "../../../assets/images/union.svg";
 import finishCharacter from "../../../assets/images/finish_character.svg";
 
-import SkipModal from "../SkipModal/SkipModal";
-
 import { useState } from "react";
 
 export default function OnboardingStatus({
@@ -14,15 +12,13 @@ export default function OnboardingStatus({
   step = "info",
   onBack,
 }) {
-  const [isSkipModalOpen, setIsSkipModalOpen] = useState(false);
-
   return (
     <div>
       {onBack && (
         <img
           src={backButton}
           onClick={onBack}
-          className="absolute left-[20px] top-0 cursor-pointer"
+          className="ml-[20px] pt-[25px] cursor-pointer"
         />
       )}
       {step === "return" && (
@@ -33,12 +29,7 @@ export default function OnboardingStatus({
           </div>
         </div>
       )}
-      <p
-        onClick={() => setIsSkipModalOpen(true)}
-        className="text-zinc-300 text-xs font-medium underline leading-5 mt-[69px] ml-[319px] cursor-pointer"
-      >
-        건너뛰기
-      </p>
+     
       <div className="relative w-[330px] h-[10px] bg-neutral-100 rounded-lg mt-[84px] ml-[30px]">
         {/* 진행바 */}
         <div
@@ -58,17 +49,6 @@ export default function OnboardingStatus({
           <img src={flagIcon} className="absolute -top-[37px] right-0" />
         )}
       </div>
-      {isSkipModalOpen && (
-        <SkipModal
-          onClose={() => setIsSkipModalOpen(false)}
-          onSkip={() => {
-            setIsSkipModalOpen(false);
-          }}
-          onContinue={() => {
-            setIsSkipModalOpen(false);
-          }}
-        />
-      )}
     </div>
   );
 }
